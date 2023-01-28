2f3dc3c2e1ae1318ea0dc4e482d86f426103e0eb
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-28 21:52:51 +0000

    moving the function into a searchBtn

--- assets/index.js
+++ assets/index.js
@@ -1,77 +1,80 @@
-let todaysWeather = document.getElementById('todaysWeather');
+let todaysWeather = document.getElementById("todaysWeather");
+let searcBtn = document.getElementById("search-button");
 
-let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=3a26ce967f024afe0e2f03c5159310b9"
+searcBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let queryURL =
+    "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=3a26ce967f024afe0e2f03c5159310b9";
 
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((cityList) => {
+      let city = cityList[0];
+      //   console.log(city.lat);
+      //   console.log(city.lon);
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&speed=miles/hour&temp=celcius&appid=3a26ce967f024afe0e2f03c5159310b9`
+      );
+    })
+    .then((response) => response.json())
+    .then((weather) => {
+      let result = weather.list;
+      let requiredIndexes = [2, 10, 18, 26, 34, 39];
+      let filteredResult = requiredIndexes.map((index) => result[index]);
 
-fetch(queryURL)
-        .then((response) => response.json())
-        .then((cityList) => {
-          let city = cityList[0]
-        //   console.log(city.lat);
-        //   console.log(city.lon);
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&speed=miles/hour&temp=celcius&appid=3a26ce967f024afe0e2f03c5159310b9`)
-})
+      // console.log(filteredResult); this returns the 6 arrays I need to loop through and display the weather stats
 
-.then(response => response.json())
-.then(weather => {
+      // console.log(weather);this returns the results
 
-    let result = weather.list;
-    let requiredIndexes = [2,10,18,26,34,39];
-    let filteredResult = requiredIndexes.map(index => result[index]);
-    
-    
-    // console.log(filteredResult); this returns the 6 arrays I need to loop through and display the weather stats
+      let test = moment(1674874800, "X").format("DD/MM/YYYY HH:mm:ss"); // this converts the DT unix timestamp
 
-    // console.log(weather);this returns the results
+      let cardDate = document.querySelectorAll("#card-date");
+      let weatherIcon = document.querySelectorAll(".weatherIcon");
+      let cardTemp = document.querySelectorAll("#card-temp");
+      let cardWind = document.querySelectorAll("#card-wind");
+      let cardHumid = document.querySelectorAll("#card-humid");
 
-    let test = moment(1674874800, "X").format("DD/MM/YYYY HH:mm:ss");// this converts the DT unix timestamp
-
-    let cardDate = document.querySelectorAll('#card-date');
-    let weatherIcon = document.querySelectorAll('.weatherIcon');
-    let cardTemp = document.querySelectorAll('#card-temp');
-    let cardWind = document.querySelectorAll('#card-wind');
-    let cardHumid = document.querySelectorAll('#card-humid');
-
-    
-    for (let i = 0; i < filteredResult.length; i++) {
+      for (let i = 0; i < filteredResult.length; i++) {
         const weather = filteredResult[i];
         console.log(weather);
-        let icons = weather.weather[0].icon
+        let icons = weather.weather[0].icon;
         console.log(icons);
         cardDate[i].textContent = moment(weather.dt, "X").format("DD/MM/YYYY");
-        weatherIcon[i].setAttribute("src", "http://openweathermap.org/img/w/" + icons + ".png")
-        cardTemp[i].textContent = "Temp " + weather.main.temp + " °C"
+        weatherIcon[i].setAttribute(
+          "src",
+          "http://openweathermap.org/img/w/" + icons + ".png"
+        );
+        cardTemp[i].textContent = "Temp " + weather.main.temp + " °C";
         cardWind[i].textContent = "Wind " + weather.wind.speed + " MPH";
         cardHumid[i].textContent = "Humidity " + weather.main.humidity + " %";
-    }
-})
+      }
+    });
+});
+
+// console.log(filteredResult); this returns the 6 arrays I need to loop through and display the weather stats
+
+// console.log(weather);this returns the results
 
 //weather.weather[0].icon
 //weather.main.temp
 //weather.wind.speed
 //weather.main.humidity
 
-
 //PSEUDO CODE//
 
+// I've created the required function to show both the weather on the current day as well as the five days for a predetermined city.
 
-// I've created the required function to show both the weather on the current day as well as the five days for a predetermined city. 
-
- //I need to show these required values from the current day as well as the next 5 days.
+//I need to show these required values from the current day as well as the next 5 days.
 
 //after this is done I need to workout how to take the city input from the user and apply this to the functions I already have.
 
-//Once that is done, I need to store the results in local storage and show these in the recent searches. 
+//Once that is done, I need to store the results in local storage and show these in the recent searches.
 
 //When the user clicks on one of the results in the search history it should update and show the weather stats for that city.
 
-
-
-
-
 //for 6pm snapshot each day
 
-//I need indexes 
+//I need indexes
 // 6 for day 1, 14 for day 2, 22 for day 3, 30 for day 4, 38 for day 5
 
 // let queryURL2 = (`https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219
@@ -84,16 +87,15 @@ fetch(queryURL)
 //     console.log(forecast)
 // })
 
-
 //use moment JS to display the time?
 
-   // let newDiv = document.createElement('div');
-    // newDiv.innerHTML = 
-    // `<h5>${weather.name}</h5>
-    // <img src="http://openweathermap.org/img/w/10d.png
-    // "${weather.weather(0).icon}">
-    // <p>Wind: ${weather.wind.speed}MPH</p>
-    // <p>Temp: ${weather.main.temp}</p>
-    // <p>Humidity:${weather.main.humidity}%</p>`
+// let newDiv = document.createElement('div');
+// newDiv.innerHTML =
+// `<h5>${weather.name}</h5>
+// <img src="http://openweathermap.org/img/w/10d.png
+// "${weather.weather(0).icon}">
+// <p>Wind: ${weather.wind.speed}MPH</p>
+// <p>Temp: ${weather.main.temp}</p>
+// <p>Humidity:${weather.main.humidity}%</p>`
 
-    // todaysWeather.append(newDiv);
+// todaysWeather.append(newDiv);
