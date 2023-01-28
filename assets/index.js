c6b4c48b5dcb69133cb3f1d2a07de94b070ac7ed
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-28 01:47:16 +0000

    creating draft cards to hold 5 day forecast, I've also mapped over the api forecast endpoint and retrieved the required indexes and will be looping through these and display them in the required places

--- assets/index.js
+++ assets/index.js
@@ -14,10 +14,26 @@ fetch(queryURL)
 
 .then(response => response.json())
 .then(weather => {
+
+    let result = weather.list;
+    let requiredIndexes = [2,10,18,26,34,39];
+    let filteredResult = requiredIndexes.map(index => result[index]);
+    console.log(filteredResult);
+
     console.log(weather);
 
- 
+    let test = moment(1674874800, "X").format("DD/MM/YYYY HH:mm:ss");// this converts the DT unix timestamp
+    
+    for (let i = 0; i < filteredResult.length; i++) {
+        const index = filteredResult[i];
+        console.log(index)
+    }
 })
+
+//for 6pm snapshot each day
+
+//I need indexes 
+// 6 for day 1, 14 for day 2, 22 for day 3, 30 for day 4, 38 for day 5
 
 // let queryURL2 = (`https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219
 // &lon=-0.1276474
